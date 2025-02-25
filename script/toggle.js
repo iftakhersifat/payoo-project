ff17a38208b3beea1fad6ced3a78793dda6c3612
iftakhersifat <hossensifat615@gmail.com>
2025-02-26 01:00:35 +0600

    added transaction history toggle

--- script/toggle.js
+++ script/toggle.js
@@ -1,13 +1,18 @@
 document.getElementById('add-money-box').addEventListener('click', function(){
     document.getElementById('cashOut').style.display="none";
     document.getElementById('addMoney').style.display='block';
-    
+    document.getElementById('transaction').style.display = 'none';
     
 })
 document.getElementById('cashOut-box').addEventListener('click', function(){
     document.getElementById('cashOut').style.display="block";
     document.getElementById('addMoney').style.display='none';
-    
+    document.getElementById('transaction').style.display = 'none';
     
 })
 
+document.getElementById('transaction-box').addEventListener('click', function(){
+    document.getElementById('cashOut').style.display = 'none';
+    document.getElementById('addMoney').style.display = 'none';
+    document.getElementById('transaction').style.display = 'block';
+})
