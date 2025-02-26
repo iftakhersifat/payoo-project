d5fe15b14f8bab063b5b07ee5cc6449c1e787039
iftakhersifat <hossensifat615@gmail.com>
2025-02-26 12:09:11 +0600

    updated toggle section

--- script/toggle.js
+++ script/toggle.js
@@ -1,3 +1,7 @@
+document.getElementById('cashOut').style.display="none";
+document.getElementById('transaction').style.display = 'none';
+
+
 document.getElementById('add-money-box').addEventListener('click', function(){
     document.getElementById('cashOut').style.display="none";
     document.getElementById('addMoney').style.display='block';
