f3f660a2dd6adaaca7524932668ea83f87ee0fcc
iftakhersifat <hossensifat615@gmail.com>
2025-02-26 18:04:03 +0600

    added bonus togggle

--- script/toggle.js
+++ script/toggle.js
@@ -1,6 +1,7 @@
 document.getElementById('cashOut').style.display="none";
 document.getElementById('transaction').style.display = 'none';
 document.getElementById('sendMoney').style.display = 'none';
+document.getElementById('get-bonus').style.display = 'none';
 
 
 document.getElementById('add-money-box').addEventListener('click', function(){
@@ -8,6 +9,7 @@ document.getElementById('add-money-box').addEventListener('click', function(){
     document.getElementById('addMoney').style.display='block';
     document.getElementById('transaction').style.display = 'none';
     document.getElementById('sendMoney').style.display = 'none';
+    document.getElementById('get-bonus').style.display = 'none';
     
 })
 document.getElementById('cashOut-box').addEventListener('click', function(){
@@ -15,6 +17,7 @@ document.getElementById('cashOut-box').addEventListener('click', function(){
     document.getElementById('addMoney').style.display='none';
     document.getElementById('transaction').style.display = 'none';
     document.getElementById('sendMoney').style.display = 'none';
+    document.getElementById('get-bonus').style.display = 'none';
     
 })
 
@@ -23,6 +26,7 @@ document.getElementById('transaction-box').addEventListener('click', function(){
     document.getElementById('addMoney').style.display = 'none';
     document.getElementById('transaction').style.display = 'block';
     document.getElementById('sendMoney').style.display = 'none';
+    document.getElementById('get-bonus').style.display = 'none';
 })
 
 document.getElementById('sendMoney-card').addEventListener('click', function(){
@@ -30,5 +34,14 @@ document.getElementById('sendMoney-card').addEventListener('click', function(){
     document.getElementById('addMoney').style.display = 'none';
     document.getElementById('transaction').style.display = 'none';
     document.getElementById('sendMoney').style.display = 'block';
+    document.getElementById('get-bonus').style.display = 'none';
+})
+
+document.getElementById('bonus-card').addEventListener('click', function(){
+    document.getElementById('cashOut').style.display = 'none';
+    document.getElementById('addMoney').style.display = 'none';
+    document.getElementById('transaction').style.display = 'none';
+    document.getElementById('sendMoney').style.display = 'none';
+    document.getElementById('get-bonus').style.display = 'block';
 })
 
